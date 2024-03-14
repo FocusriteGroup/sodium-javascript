56c21315abf82869cb83c83bd8a97b81146c870b
Matt Richardson <matt.richardson@focusrite.com>
2024-03-14 16:10:29 +0000

    export session key bytes

--- crypto_kx.js
+++ crypto_kx.js
@@ -1,64 +1,102 @@
 /* eslint-disable camelcase */
-const { crypto_scalarmult_BYTES, crypto_scalarmult_base, crypto_scalarmult } = require('./crypto_scalarmult')
-const { crypto_generichash } = require('./crypto_generichash')
-const { randombytes_buf } = require('./randombytes')
-const { sodium_memzero } = require('./memory')
-const assert = require('nanoassert')
+const {
+  crypto_scalarmult_BYTES,
+  crypto_scalarmult_base,
+  crypto_scalarmult,
+} = require("./crypto_scalarmult");
+const { crypto_generichash } = require("./crypto_generichash");
+const { randombytes_buf } = require("./randombytes");
+const { sodium_memzero } = require("./memory");
+const assert = require("nanoassert");
 
-const crypto_kx_SEEDBYTES = 32
-const crypto_kx_PUBLICKEYBYTES = 32
-const crypto_kx_SECRETKEYBYTES = 32
+const crypto_kx_SEEDBYTES = 32;
+const crypto_kx_PUBLICKEYBYTES = 32;
+const crypto_kx_SECRETKEYBYTES = 32;
 const crypto_kx_SESSIONKEYBYTES = 32;
 
-function crypto_kx_keypair (pk, sk) {
-  assert(pk.byteLength === crypto_kx_PUBLICKEYBYTES, "pk must be 'crypto_kx_PUBLICKEYBYTES' bytes")
-  assert(sk.byteLength === crypto_kx_SECRETKEYBYTES, "sk must be 'crypto_kx_SECRETKEYBYTES' bytes")
+function crypto_kx_keypair(pk, sk) {
+  assert(
+    pk.byteLength === crypto_kx_PUBLICKEYBYTES,
+    "pk must be 'crypto_kx_PUBLICKEYBYTES' bytes"
+  );
+  assert(
+    sk.byteLength === crypto_kx_SECRETKEYBYTES,
+    "sk must be 'crypto_kx_SECRETKEYBYTES' bytes"
+  );
 
-  randombytes_buf(sk, crypto_kx_SECRETKEYBYTES)
-  return crypto_scalarmult_base(pk, sk)
+  randombytes_buf(sk, crypto_kx_SECRETKEYBYTES);
+  return crypto_scalarmult_base(pk, sk);
 }
 
-function crypto_kx_seed_keypair (pk, sk, seed) {
-  assert(pk.byteLength === crypto_kx_PUBLICKEYBYTES, "pk must be 'crypto_kx_PUBLICKEYBYTES' bytes")
-  assert(sk.byteLength === crypto_kx_SECRETKEYBYTES, "sk must be 'crypto_kx_SECRETKEYBYTES' bytes")
-  assert(seed.byteLength === crypto_kx_SEEDBYTES, "seed must be 'crypto_kx_SEEDBYTES' bytes")
+function crypto_kx_seed_keypair(pk, sk, seed) {
+  assert(
+    pk.byteLength === crypto_kx_PUBLICKEYBYTES,
+    "pk must be 'crypto_kx_PUBLICKEYBYTES' bytes"
+  );
+  assert(
+    sk.byteLength === crypto_kx_SECRETKEYBYTES,
+    "sk must be 'crypto_kx_SECRETKEYBYTES' bytes"
+  );
+  assert(
+    seed.byteLength === crypto_kx_SEEDBYTES,
+    "seed must be 'crypto_kx_SEEDBYTES' bytes"
+  );
 
-  crypto_generichash(sk, seed)
-  return crypto_scalarmult_base(pk, sk)
+  crypto_generichash(sk, seed);
+  return crypto_scalarmult_base(pk, sk);
 }
 
-function crypto_kx_client_session_keys(rx, tx, client_pk, client_sk, server_pk) {
-  assert(client_pk.byteLength === crypto_kx_PUBLICKEYBYTES, "client_pk must be 'crypto_kx_PUBLICKEYBYTES' bytes");
-  assert(client_sk.byteLength === crypto_kx_SECRETKEYBYTES, "client_sk must be 'crypto_kx_SECRETKEYBYTES' bytes");
-  assert(server_pk.byteLength === crypto_kx_PUBLICKEYBYTES, "server_pk must be 'crypto_kx_PUBLICKEYBYTES' bytes");
+function crypto_kx_client_session_keys(
+  rx,
+  tx,
+  client_pk,
+  client_sk,
+  server_pk
+) {
+  assert(
+    client_pk.byteLength === crypto_kx_PUBLICKEYBYTES,
+    "client_pk must be 'crypto_kx_PUBLICKEYBYTES' bytes"
+  );
+  assert(
+    client_sk.byteLength === crypto_kx_SECRETKEYBYTES,
+    "client_sk must be 'crypto_kx_SECRETKEYBYTES' bytes"
+  );
+  assert(
+    server_pk.byteLength === crypto_kx_PUBLICKEYBYTES,
+    "server_pk must be 'crypto_kx_PUBLICKEYBYTES' bytes"
+  );
 
-  if (!rx){
+  if (!rx) {
     rx = tx;
   }
 
-  if (!tx){
+  if (!tx) {
     tx = rx;
   }
 
-  if (!rx){
-    throw new Error('requires session key bytes');
+  if (!rx) {
+    throw new Error("requires session key bytes");
   }
 
   const q = new Uint8Array(crypto_scalarmult_BYTES);
   if (crypto_scalarmult(q, client_sk, server_pk) !== 0) {
-    throw new Error('crypto_scalarmult failed');
+    throw new Error("crypto_scalarmult failed");
   }
 
   const keys = new Uint8Array(2 * crypto_kx_SESSIONKEYBYTES);
-  const hashInput = new Uint8Array(q.length + client_pk.length + server_pk.length);
+  const hashInput = new Uint8Array(
+    q.length + client_pk.length + server_pk.length
+  );
   hashInput.set(q, 0);
   hashInput.set(client_pk, q.length);
   hashInput.set(server_pk, q.length + client_pk.length);
 
   crypto_generichash(keys, hashInput);
-  
+
   rx.set(keys.subarray(0, crypto_kx_SESSIONKEYBYTES));
-  tx.set(keys.subarray(crypto_kx_SESSIONKEYBYTES, 2 * crypto_kx_SESSIONKEYBYTES));
+  tx.set(
+    keys.subarray(crypto_kx_SESSIONKEYBYTES, 2 * crypto_kx_SESSIONKEYBYTES)
+  );
 
   sodium_memzero(q);
   sodium_memzero(keys);
@@ -72,5 +110,6 @@ module.exports = {
   crypto_kx_client_session_keys,
   crypto_kx_SEEDBYTES,
   crypto_kx_SECRETKEYBYTES,
-  crypto_kx_PUBLICKEYBYTES
-}
+  crypto_kx_PUBLICKEYBYTES,
+  crypto_kx_SESSIONKEYBYTES,
+};
